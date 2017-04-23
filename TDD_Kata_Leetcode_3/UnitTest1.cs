6b9af6e59d5c6ce4c379675f92666d39a4d74dae
JulianChu <yulang.chu@gmail.com>
2017-04-23 16:05:06 +0200

    add passed test case: string="aba", return 2

--- TDD_Kata_Leetcode_3/UnitTest1.cs
+++ TDD_Kata_Leetcode_3/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
 namespace TDD_Kata_Leetcode_3
@@ -23,27 +22,37 @@ namespace TDD_Kata_Leetcode_3
         [TestMethod]
         public void LengthOfLongestSubstring_Give_string_a_Returns_1()
         {
-            //Assign 
+            //Assign
             string target = "a";
             //Act
             int actual = LengthOfLongestSubstring(target);
             //Assert
             int expected = 1;
             Assert.AreEqual(expected, actual);
-
         }
 
         [TestMethod]
         public void LengthOfLongestSubstring_Give_string_aa_Return_1()
         {
-            //Assign 
+            //Assign
             string target = "aa";
             //Act
             int actual = LengthOfLongestSubstring(target);
             //Assert
             int expected = 1;
             Assert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        public void LengthOfLongestSubstring_Give_string_aba_Return_2()
+        {
+            //Assign
+            string target = "aba";
+            //Act
+            int actual = LengthOfLongestSubstring(target);
+            //Assert
+            int expected = 2;
+            Assert.AreEqual(expected, actual);
         }
 
         private int LengthOfLongestSubstring(string target)
